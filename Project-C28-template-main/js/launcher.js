ac351745bc511705905061baa00d4446e39d3530
ShreyasKowshik <76945179+ShreyasKowshik@users.noreply.github.com>
2021-04-22 16:29:19 +0530

    Add files via upload

---
+++ Project-C28-template-main/js/launcher.js
@@ -0,0 +1,26 @@
+  
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.launche1 = Constraint.create(options);
+        World.add(world, this.launche1);
+    }
+
+    display(){
+        if (this.launche1.bodyA != null) {
+            var pointA = this.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);    
+        }
+    }
+    fly(){
+    this.launche1.bodyA = null
+    }
+}
